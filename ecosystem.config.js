9a0539f791bf0d2c4ea5377a38d2fa85a18bf245
hjkangIB <hjkang@idealbloom.io>
2022-08-10 13:46:45 +0900

    chore: 프로젝트 명 변경 및 포트, 데이터베이스명 세팅

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,31 +1,31 @@
 module.exports = {
-    apps: [
-      {
-        name: 'ts-prisma-restful-bp',
-        script: 'ts-node -r tsconfig-paths/register ./src/server.ts --watch',
-        time: true,
-        watch: true,
-        // TZ: 'Asia/Seoul',
-        // restart_delay: 5000,
-        max_restarts: 5,
-        min_uptime: 5000,
-        instance_var: 'INSTANCE_ID',
-        env: {
-          "PORT": 3000,
-          "NODE_ENV": 'development',
-        },
-        env_production: {
-          "PORT": 3100,
-          "NODE_ENV": 'production',
-        },
-        env_local: {
-          "PORT": 3200,
-          "NODE_ENV": 'local',
-        },
-        env_test: {
-          "PORT": 3300,
-          "NODE_ENV": 'test',
-        },
+  apps: [
+    {
+      name: 'travelit-api',
+      script: 'ts-node -r tsconfig-paths/register ./src/server.ts --watch',
+      time: true,
+      watch: true,
+      // TZ: 'Asia/Seoul',
+      // restart_delay: 5000,
+      max_restarts: 5,
+      min_uptime: 5000,
+      instance_var: 'INSTANCE_ID',
+      env: {
+        PORT: 3000,
+        NODE_ENV: 'development',
       },
-    ],
+      env_production: {
+        PORT: 3100,
+        NODE_ENV: 'production',
+      },
+      env_local: {
+        PORT: 3200,
+        NODE_ENV: 'local',
+      },
+      env_test: {
+        PORT: 3300,
+        NODE_ENV: 'test',
+      },
+    },
+  ],
 };
