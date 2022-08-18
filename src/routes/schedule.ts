9cfe3ce25d6b86ccc501f48dc1f1240f53b25f41
hjkangIB <hjkang@idealbloom.io>
2022-08-18 17:42:27 +0900

    feat: /searchHotel /nearbySearch 함께 실행할 api를 만들기 위해 각각 inner 함수를 생성하고 내부 로직 분리

--- src/routes/schedule.ts
+++ src/routes/schedule.ts
@@ -10,103 +10,105 @@ import moment from 'moment';
 
 const scheduleRouter: express.Application = express();
 
+type NearBySearchParams = {
+  keyword: string;
+  location: {
+    latitude: string; // 위도
+    longitude: string; // 경도
+  };
+  radius: number;
+  pageToken: string;
+};
+
+const nearbySearchInnerAsyncFn = async (param: NearBySearchParams) => {
+  const { location, radius, pageToken, keyword } = param;
+  const queryUrl = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?keyword=${keyword}&location=${
+    location?.latitude
+  }%2C${location?.longitude}&radius=${radius}&key=${
+    process.env.GCP_MAPS_APIKEY as string
+  }${pageToken ? `&pagetoken=${pageToken}` : ''}`;
+  console.log(queryUrl);
+  const response = await axios.get(queryUrl);
+  if (response?.statusText === 'OK') {
+    // const results = response.data as Partial<{
+    //   result: GglNearbySearchRes[];
+    // }>;
+    // const results: google.maps.places.PlaceResult = response.data;
+    const { results } = response.data as Partial<{
+      results: google.maps.places.IBPlaceResult[];
+    }>;
+
+    const promises = results?.map((item: google.maps.places.IBPlaceResult) => {
+      return prisma.gglNearbySearchRes.create({
+        data: {
+          geometry: {
+            create: {
+              location: JSON.stringify({
+                lat: item.geometry?.location?.lat,
+                lng: item.geometry?.location?.lng,
+              }),
+              viewport: JSON.stringify({
+                northeast: {
+                  lat: item.geometry?.viewport?.northeast?.lat,
+                  lng: item.geometry?.viewport?.northeast?.lng,
+                },
+                southwest: {
+                  lat: item.geometry?.viewport?.southwest?.lat,
+                  lng: item.geometry?.viewport?.southwest?.lng,
+                },
+              }),
+            },
+          },
+          icon: item.icon,
+          icon_background_color: item.icon_background_color,
+          icon_mask_base_uri: item.icon_mask_base_uri,
+          name: item.name,
+          opening_hours:
+            (
+              item.opening_hours as Partial<{
+                open_now: boolean;
+              }>
+            )?.open_now ?? false,
+          place_id: item.place_id,
+          price_level: item.price_level,
+          rating: item.rating,
+          types: JSON.stringify(item.types),
+          user_ratings_total: item.user_ratings_total,
+          vicinity: item.vicinity,
+          plus_code: {
+            create: {
+              compund_code: item.plus_code?.compound_code ?? '',
+              global_code: item.plus_code?.global_code ?? '',
+            },
+          },
+          photos: {
+            create: item.photos?.map(photo => {
+              return {
+                height: photo.height,
+                width: photo.width,
+                html_attributuions: JSON.stringify(photo.html_attributions),
+                photo_reference:
+                  (photo as Partial<{ photo_reference: string }>)
+                    .photo_reference ?? '',
+              };
+            }),
+          },
+        },
+      });
+    });
+
+    if (promises) await Promise.all(promises);
+  }
+  console.log(JSON.stringify(response.data, null, 2));
+  return response;
+};
+
 export const nearbySearch = asyncWrapper(
   async (
-    req: Express.IBTypedReqBody<{
-      keyword: string;
-      location: {
-        latitude: string; // 위도
-        longitude: string; // 경도
-      };
-      radius: number;
-      pageToken: string;
-    }>,
+    req: Express.IBTypedReqBody<NearBySearchParams>,
     res: Express.IBTypedResponse<IBResFormat>,
   ) => {
-    const {
-      body: { keyword, location, radius, pageToken },
-    } = req;
-
-    const queryUrl = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?keyword=${keyword}&location=${
-      location?.latitude
-    }%2C${location?.longitude}&radius=${radius}&key=${
-      process.env.GCP_MAPS_APIKEY as string
-    }${pageToken ? `&pagetoken=${pageToken}` : ''}`;
-    console.log(queryUrl);
-    const response = await axios.get(queryUrl);
-    if (response?.statusText === 'OK') {
-      // const results = response.data as Partial<{
-      //   result: GglNearbySearchRes[];
-      // }>;
-      // const results: google.maps.places.PlaceResult = response.data;
-      const { results } = response.data as Partial<{
-        results: google.maps.places.IBPlaceResult[];
-      }>;
-
-      const promises = results?.map(
-        (item: google.maps.places.IBPlaceResult) => {
-          return prisma.gglNearbySearchRes.create({
-            data: {
-              geometry: {
-                create: {
-                  location: JSON.stringify({
-                    lat: item.geometry?.location?.lat,
-                    lng: item.geometry?.location?.lng,
-                  }),
-                  viewport: JSON.stringify({
-                    northeast: {
-                      lat: item.geometry?.viewport?.northeast?.lat,
-                      lng: item.geometry?.viewport?.northeast?.lng,
-                    },
-                    southwest: {
-                      lat: item.geometry?.viewport?.southwest?.lat,
-                      lng: item.geometry?.viewport?.southwest?.lng,
-                    },
-                  }),
-                },
-              },
-              icon: item.icon,
-              icon_background_color: item.icon_background_color,
-              icon_mask_base_uri: item.icon_mask_base_uri,
-              name: item.name,
-              opening_hours:
-                (
-                  item.opening_hours as Partial<{
-                    open_now: boolean;
-                  }>
-                )?.open_now ?? false,
-              place_id: item.place_id,
-              price_level: item.price_level,
-              rating: item.rating,
-              types: JSON.stringify(item.types),
-              user_ratings_total: item.user_ratings_total,
-              vicinity: item.vicinity,
-              plus_code: {
-                create: {
-                  compund_code: item.plus_code?.compound_code ?? '',
-                  global_code: item.plus_code?.global_code ?? '',
-                },
-              },
-              photos: {
-                create: item.photos?.map(photo => {
-                  return {
-                    height: photo.height,
-                    width: photo.width,
-                    html_attributuions: JSON.stringify(photo.html_attributions),
-                    photo_reference:
-                      (photo as Partial<{ photo_reference: string }>)
-                        .photo_reference ?? '',
-                  };
-                }),
-              },
-            },
-          });
-        },
-      );
-
-      if (promises) await Promise.all(promises);
-    }
-    console.log(JSON.stringify(response.data, null, 2));
+    const response = await nearbySearchInnerAsyncFn(req.body);
     res.json({
       ...ibDefs.SUCCESS,
       IBparams: response.data as object,
