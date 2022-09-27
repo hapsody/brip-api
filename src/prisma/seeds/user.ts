ee5d1419e802f32e36556c460f915be430222173
hjkangIB <hjkang@idealbloom.io>
2022-09-27 17:44:34 +0900

    chore: user seed 비밀번호 암호화 하지 않던 오류 수정

--- src/prisma/seeds/user.ts
+++ src/prisma/seeds/user.ts
@@ -1,19 +1,23 @@
 import { PrismaClient } from '@prisma/client';
+import { genBcryptHash } from '../../utils';
 
 const prisma = new PrismaClient();
 
+const userData = {
+  email: 'hawaii@gmail.com',
+  password: 'qwer1234',
+  phone: '+821012345678',
+  nickName: '유쾌한인어',
+  countryCode: 'KR',
+  userTokenId: '0',
+};
+
 async function main() {
+  const hash = genBcryptHash(userData.password);
   const exampleUser = await prisma.user.upsert({
     where: { email: 'hawaii@gmail.com' },
     update: {},
-    create: {
-      email: 'hawaii@gmail.com',
-      password: 'qwer1234',
-      phone: '+821012345678',
-      nickName: '유쾌한인어',
-      countryCode: 'KR',
-      userTokenId: '0',
-    },
+    create: { ...userData, password: hash },
   });
   console.log(exampleUser);
 }
