4d262968148c9d87a117a6700b0cfeafa6a096af
hjkangIB <hjkang@idealbloom.io>
2022-10-05 15:45:11 +0900

    feat: 1. reqBusinessTicket api 추가, 2. BusinessQuestionTicket model 생성

--- src/routes/setting/setting.ts
+++ src/routes/setting/setting.ts
@@ -105,6 +105,106 @@ export const reqTicket = asyncWrapper(
   },
 );
 
+export type ReqBusinessTicketRequestType = {
+  name: string;
+  phone: string;
+  content: string;
+};
+// export interface ReqBusinessTicketSuccessResType {
+//   groupNo: number;
+//   groupId: number;
+//   groupTitle: string;
+//   groupThumbnail: string;
+//   cards: {
+//     cardId: number;
+//     cardNo: number;
+//     tag: CardTag[];
+//     cardTitle: string;
+//     cardContent: string;
+//     cardBgUri: string;
+//   }[];
+// }
+
+export type ReqBusinessTicketResType = Omit<IBResFormat, 'IBparams'> & {
+  // IBparams: ReqBusinessTicketSuccessResType[] | {};
+  IBparams: {};
+};
+
+export const reqBusinessTicket = asyncWrapper(
+  async (
+    req: Express.IBTypedReqBody<ReqBusinessTicketRequestType>,
+    res: Express.IBTypedResponse<ReqBusinessTicketResType>,
+  ) => {
+    try {
+      const { locals } = req;
+      const userTokenId = (() => {
+        if (locals && locals?.grade === 'member')
+          return locals?.user?.userTokenId;
+        // return locals?.tokenId;
+        throw new IBError({
+          type: 'NOTAUTHORIZED',
+          message: 'member 등급만 접근 가능합니다.',
+        });
+      })();
+      if (!userTokenId) {
+        throw new IBError({
+          type: 'NOTEXISTDATA',
+          message: '정상적으로 부여된 userTokenId를 가지고 있지 않습니다.',
+        });
+      }
+      const { name, phone, content } = req.body;
+
+      await prisma.businessQuestionTicket.create({
+        data: {
+          companyName: name,
+          phone,
+          content,
+          user: {
+            connect: {
+              userTokenId,
+            },
+          },
+        },
+      });
+
+      res.json({
+        ...ibDefs.SUCCESS,
+        IBparams: {},
+      });
+    } catch (err) {
+      if (err instanceof IBError) {
+        // if (err.type === 'INVALIDENVPARAMS') {
+        //   res.status(500).json({
+        //     ...ibDefs.INVALIDENVPARAMS,
+        //     IBdetail: (err as Error).message,
+        //     IBparams: {} as object,
+        //   });
+        //   return;
+        // }
+
+        if (err.type === 'NOTAUTHORIZED') {
+          res.status(403).json({
+            ...ibDefs.NOTAUTHORIZED,
+            IBdetail: (err as Error).message,
+            IBparams: {} as object,
+          });
+          return;
+        }
+        if (err.type === 'NOTEXISTDATA') {
+          res.status(202).json({
+            ...ibDefs.NOTEXISTDATA,
+            IBdetail: (err as Error).message,
+            IBparams: {} as object,
+          });
+          return;
+        }
+      }
+      throw err;
+    }
+  },
+);
+
 authRouter.post('/reqTicket', accessTokenValidCheck, reqTicket);
+authRouter.post('/reqBusinessTicket', accessTokenValidCheck, reqBusinessTicket);
 
 export default authRouter;
