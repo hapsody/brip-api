2a28dfc2fca36117525fd534f4bd6276854a86fd
hjkangIB <hjkang@idealbloom.io>
2022-10-06 11:23:10 +0900

    feat: 1. reqTripCreator api 추가, 2. TripCreator model 스키마 추가

--- src/routes/setting/setting.ts
+++ src/routes/setting/setting.ts
@@ -278,8 +278,151 @@ export const getFaqList = asyncWrapper(
   },
 );
 
+enum CreatorDomain {
+  TOUR,
+  ACTIVITY,
+  HIKING,
+  HONEYMOON,
+  GOLF,
+  PACKAGE,
+  EXPERIENCE,
+  GROUPACTIVITY,
+}
+
+export type ReqTripCreatorRequestType = {
+  name: string;
+  phone: string;
+  area: string;
+  content: string;
+  checkList: (keyof typeof CreatorDomain)[];
+  // userToken: string;
+};
+// export interface ReqTripCreatorSuccessResType {
+//   title: string;
+//   answer: string;
+// }
+
+export type ReqTripCreatorResType = Omit<IBResFormat, 'IBparams'> & {
+  // IBparams: ReqTripCreatorSuccessResType[] | {};
+  IBparams: {};
+};
+
+export const reqTripCreator = asyncWrapper(
+  async (
+    req: Express.IBTypedReqBody<ReqTripCreatorRequestType>,
+    res: Express.IBTypedResponse<ReqTripCreatorResType>,
+  ) => {
+    try {
+      const { locals } = req;
+      const userTokenId = (() => {
+        if (locals && locals?.grade === 'member')
+          return locals?.user?.userTokenId;
+        // return locals?.tokenId;
+        throw new IBError({
+          type: 'NOTAUTHORIZED',
+          message: 'member 등급만 접근 가능합니다.',
+        });
+      })();
+
+      if (!userTokenId) {
+        throw new IBError({
+          type: 'NOTEXISTDATA',
+          message: '정상적으로 부여된 userTokenId를 가지고 있지 않습니다.',
+        });
+      }
+      const { name, phone, area, content, checkList } = req.body;
+
+      try {
+        const alreadyCreator = await prisma.tripCreator.findUnique({
+          where: {
+            nickName: name,
+          },
+        });
+
+        if (alreadyCreator) {
+          throw new IBError({
+            type: 'DUPLICATEDDATA',
+            message: '이미 존재하는 nickname입니다.',
+          });
+        }
+
+        await prisma.tripCreator.create({
+          data: {
+            nickName: name,
+            phone,
+            area,
+            proposal: content,
+            domain: checkList
+              .map(
+                v =>
+                  CreatorDomain[v.toUpperCase() as keyof typeof CreatorDomain],
+              )
+              .toString(),
+            user: {
+              connect: {
+                userTokenId,
+              },
+            },
+          },
+        });
+      } catch (err) {
+        if (err instanceof IBError) {
+          throw err;
+        }
+        throw new IBError({
+          type: 'DBTRANSACTIONERROR',
+          message: 'DB 생성중에 문제가 발생했습니다.',
+        });
+      }
+
+      res.json({
+        ...ibDefs.SUCCESS,
+        IBparams: {},
+      });
+    } catch (err) {
+      console.error(err);
+      if (err instanceof IBError) {
+        if (err.type === 'NOTAUTHORIZED') {
+          res.status(403).json({
+            ...ibDefs.NOTAUTHORIZED,
+            IBdetail: (err as Error).message,
+            IBparams: {} as object,
+          });
+          return;
+        }
+        if (err.type === 'NOTEXISTDATA') {
+          res.status(202).json({
+            ...ibDefs.NOTEXISTDATA,
+            IBdetail: (err as Error).message,
+            IBparams: {} as object,
+          });
+          return;
+        }
+        if (err.type === 'DBTRANSACTIONERROR') {
+          res.status(500).json({
+            ...ibDefs.DBTRANSACTIONERROR,
+            IBdetail: (err as Error).message,
+            IBparams: {} as object,
+          });
+          return;
+        }
+        if (err.type === 'DUPLICATEDDATA') {
+          res.status(409).json({
+            ...ibDefs.DUPLICATEDDATA,
+            IBdetail: (err as Error).message,
+            IBparams: {} as object,
+          });
+          return;
+        }
+      }
+      throw err;
+    }
+  },
+);
+
 authRouter.post('/reqTicket', accessTokenValidCheck, reqTicket);
 authRouter.post('/reqBusinessTicket', accessTokenValidCheck, reqBusinessTicket);
 authRouter.get('/getFaqList', accessTokenValidCheck, getFaqList);
+authRouter.post('/reqTripCreator', accessTokenValidCheck, reqTripCreator);
 
 export default authRouter;
