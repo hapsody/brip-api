9d3234e3e67fbd40430e31b7634c83c4d3de6420
hjkangIB <hjkang@idealbloom.io>
2023-02-23 18:51:31 +0900

    chore: api 앱 빌드, 프로덕션 실행 환경 조정

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,31 +1,65 @@
 module.exports = {
   apps: [
     {
-      name: 'travelit-api',
-      script: 'ts-node -r tsconfig-paths/register ./src/server.ts --watch',
+      name: 'travelit-api-dev',
+      script: 'ts-node -r tsconfig-paths/register ./src/server.ts',
       time: true,
       watch: true,
       // TZ: 'Asia/Seoul',
       // restart_delay: 5000,
-      max_restarts: 5,
+      max_restarts: 3,
       min_uptime: 5000,
       instance_var: 'INSTANCE_ID',
       env: {
         PORT: 3000,
         NODE_ENV: 'development',
       },
+      // env_production: {
+      //   PORT: 3000,
+      //   NODE_ENV: 'production',
+      // },
+      // env_local: {
+      //   PORT: 3000,
+      //   NODE_ENV: 'local',
+      // },
+      // env_test: {
+      //   PORT: 3000,
+      //   NODE_ENV: 'test',
+      // },
+    },
+    {
+      name: 'travelit-api-prod',
+      script:
+        'node -r ts-node/register/transpile-only -r tsconfig-paths/register dist/server.js',
+      time: true,
+      watch: true,
+      // TZ: 'Asia/Seoul',
+      // restart_delay: 5000,
+      max_restarts: 3,
+      min_uptime: 5000,
+      instance_var: 'INSTANCE_ID',
+      // env: {
+      //   PORT: 3000,
+      //   NODE_ENV: 'development',
+      // },
       env_production: {
-        PORT: 3100,
+        PORT: 3000,
         NODE_ENV: 'production',
       },
-      env_local: {
-        PORT: 3200,
-        NODE_ENV: 'local',
-      },
-      env_test: {
-        PORT: 3300,
-        NODE_ENV: 'test',
-      },
+      // env_local: {
+      //   PORT: 3000,
+      //   NODE_ENV: 'local',
+      // },
+      // env_test: {
+      //   PORT: 3000,
+      //   NODE_ENV: 'test',
+      // },
+    },
+    {
+      name: 'prismastudio',
+      script: 'prisma studio',
+      time: true,
+      watch: true,
     },
   ],
 };
