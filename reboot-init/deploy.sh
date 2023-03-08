8fc902a9a8e0636e9203d41629cca6fd786fbe80
hjkangIB <hjkang@idealbloom.io>
2023-03-08 19:14:52 +0900

    chore: deploy.sh 및 sys-resource-log.service 스크립트 수정

--- reboot-init/deploy.sh
+++ reboot-init/deploy.sh
@@ -6,8 +6,10 @@ cd /etc/systemd/system
 
 sudo systemctl daemon-reload
 
+sudo systemctl disable brip-api-server-init.service
 sudo systemctl enable brip-api-server-init.service
 sudo systemctl start brip-api-server-init.service
 
+sudo systemctl disable sys-resource-log.service
 sudo systemctl enable sys-resource-log.service
 sudo systemctl start sys-resource-log.service
