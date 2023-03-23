1a75927a9dc098eedd134a8a1e3cac849612c45d
hjkangIB <hjkang@idealbloom.io>
2023-03-23 16:50:38 +0900

    feat: openai 요청 api 추가

--- src/prisma/seeds/tourPlace/tourPlace.ts
+++ src/prisma/seeds/tourPlace/tourPlace.ts
@@ -1,36 +1,11 @@
 // import batchJob from '../../../timer/dailyBatchJob';
 
 import { PrismaClient, PlaceType, DataStageStatus } from '@prisma/client';
-import data from './data.go.kr';
+import { ValueType, value as data } from './data.go.kr';
 
 const prisma = new PrismaClient();
 
 async function main(): Promise<void> {
-  // type RegionType = keyof typeof data;
-  type ValueType = {
-    관광지명: string;
-    // 관광지구분: string;
-    소재지도로명주소?: string;
-    소재지지번주소?: string;
-    위도: number;
-    경도: number;
-    // 면적: number;
-    공공편익시설정보: string | number;
-    // 숙박시설정보?: string | number;
-    // 운동및오락시설정보?: string | number;
-    // 휴양및문화시설정보?: string | number;
-    // 접객시설정보?: string;
-    // 지원시설정보?: string;
-    // 지정일자?: string;
-    // 수용인원수?: number;
-    // 주차가능수?: number;
-    관광지소개?: string;
-    관리기관전화번호?: string;
-    // 관리기관명?: string;
-    // 데이터기준일자?: string;
-  };
-  // type DataType = Record<RegionType, ValueType[]>;
-
   const dataForCreateMany = Object.entries(data).reduce(
     (acc, record) => {
       const [, vList]: [string, ValueType[]] = record;
