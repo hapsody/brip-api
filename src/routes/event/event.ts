c646c8c8022854396e9927345de2864d55a1b106
hjkangIB <hjkang@idealbloom.io>
2023-04-05 19:03:24 +0900

    feat: hawaiiHoneymoonPack api 추가

--- src/routes/event/event.ts
+++ src/routes/event/event.ts
@@ -86,13 +86,177 @@ export const hawaiiEnglishCamp = asyncWrapper(
       if (isNil(param.phone) || isEmpty(param.phone)) {
         throw new IBError({
           type: 'INVALIDPARAMS',
-          message: 'phone는 반드시 제공되어야 하는 파라미터입니다.',
+          message: 'phone은 반드시 제공되어야 하는 파라미터입니다.',
         });
       }
 
       const pid = await prisma.iBEventPromotion.findFirst({
         where: {
           name: '하와이 영어캠프',
+        },
+      });
+
+      const createdApplication = await prisma.iBEventApplication.create({
+        data: {
+          content: JSON.stringify(param),
+          ...(isNil(pid)
+            ? {
+                promotion: {
+                  create: {
+                    name: '하와이 영어캠프',
+                    desc: '',
+                    byWhom: 'TRAVA',
+                  },
+                },
+              }
+            : {
+                promotion: {
+                  connectOrCreate: {
+                    where: {
+                      id: pid.id,
+                    },
+                    create: {
+                      name: '하와이 영어캠프',
+                      desc: '',
+                      byWhom: 'TRAVA',
+                    },
+                  },
+                },
+              }),
+          user: {
+            connect: {
+              id: userId,
+            },
+          },
+        },
+      });
+
+      res.json({
+        ...ibDefs.SUCCESS,
+        IBparams: {
+          applicationId: createdApplication.id,
+        },
+      });
+    } catch (err) {
+      if (err instanceof IBError) {
+        if (err.type === 'NOTAUTHORIZED') {
+          res.status(403).json({
+            ...ibDefs.NOTAUTHORIZED,
+            IBdetail: (err as Error).message,
+            IBparams: {} as object,
+          });
+          return;
+        }
+
+        if (err.type === 'NOTEXISTDATA') {
+          res.status(404).json({
+            ...ibDefs.NOTEXISTDATA,
+            IBdetail: (err as Error).message,
+            IBparams: {} as object,
+          });
+          return;
+        }
+
+        if (err.type === 'DBTRANSACTIONERROR') {
+          res.status(500).json({
+            ...ibDefs.DBTRANSACTIONERROR,
+            IBdetail: (err as Error).message,
+            IBparams: {} as object,
+          });
+          return;
+        }
+      }
+      throw err;
+    }
+  },
+);
+
+export type HawaiiHoneymoonPackRequestType = {
+  startDate: string; // 출발일자
+  period: string; /// 일정 선택
+  package: string; /// 패키지
+  opts: {
+    day: string;
+    value: string;
+  }[]; /// n일차 선택관광 옵션
+  freeSchedule: string[]; /// 자유일정 추가, 다중선택 가능
+  consultDate: string; /// ISOString
+  phone: string;
+};
+export interface HawaiiHoneymoonPackSuccessResType {}
+
+export type HawaiiHoneymoonPackResType = Omit<IBResFormat, 'IBparams'> & {
+  IBparams: HawaiiHoneymoonPackSuccessResType[] | {};
+};
+
+export const hawaiiHoneymoonPack = asyncWrapper(
+  async (
+    req: Express.IBTypedReqBody<HawaiiHoneymoonPackRequestType>,
+    res: Express.IBTypedResponse<HawaiiHoneymoonPackResType>,
+  ) => {
+    try {
+      const param = req.body;
+      const { locals } = req;
+      const { userTokenId, userId } = (() => {
+        if (locals && locals?.grade === 'member')
+          return {
+            userTokenId: locals?.user?.userTokenId,
+            userId: locals?.user?.id,
+          };
+        // return locals?.tokenId;
+        throw new IBError({
+          type: 'NOTAUTHORIZED',
+          message: 'member 등급만 접근 가능합니다.',
+        });
+      })();
+      if (!userTokenId) {
+        throw new IBError({
+          type: 'NOTEXISTDATA',
+          message: '정상적으로 부여된 userTokenId를 가지고 있지 않습니다.',
+        });
+      }
+
+      if (isNil(param.startDate) || isEmpty(param.startDate)) {
+        throw new IBError({
+          type: 'INVALIDPARAMS',
+          message: 'startDate는 반드시 제공되어야 하는 파라미터입니다.',
+        });
+      }
+      if (isNil(param.period) || isEmpty(param.period)) {
+        throw new IBError({
+          type: 'INVALIDPARAMS',
+          message: 'period는 반드시 제공되어야 하는 파라미터입니다.',
+        });
+      }
+      if (isNil(param.package) || isEmpty(param.package)) {
+        throw new IBError({
+          type: 'INVALIDPARAMS',
+          message: 'package는 반드시 제공되어야 하는 파라미터입니다.',
+        });
+      }
+      if (isNil(param.opts) || isEmpty(param.opts)) {
+        throw new IBError({
+          type: 'INVALIDPARAMS',
+          message: 'opts는 반드시 제공되어야 하는 파라미터입니다.',
+        });
+      }
+
+      if (isNil(param.freeSchedule) || isEmpty(param.freeSchedule)) {
+        throw new IBError({
+          type: 'INVALIDPARAMS',
+          message: 'freeSchedule은 반드시 제공되어야 하는 파라미터입니다.',
+        });
+      }
+      if (isNil(param.phone) || isEmpty(param.phone)) {
+        throw new IBError({
+          type: 'INVALIDPARAMS',
+          message: 'phone은 반드시 제공되어야 하는 파라미터입니다.',
+        });
+      }
+
+      const pid = await prisma.iBEventPromotion.findFirst({
+        where: {
+          name: '하와이 신혼여행 패키지',
         },
       });
 
@@ -175,6 +339,11 @@ settingRouter.post(
   '/hawaiiEnglishCamp',
   accessTokenValidCheck,
   hawaiiEnglishCamp,
+);
+settingRouter.post(
+  '/hawaiiHoneymoonPack',
+  accessTokenValidCheck,
+  hawaiiHoneymoonPack,
 );
 
 export default settingRouter;
