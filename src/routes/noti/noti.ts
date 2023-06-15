266c94439b7b2140556efe966df632989c0f89ee
hjkangIB <hjkang@idealbloom.io>
2023-06-15 16:11:55 +0900

    fix: sse EventSource에 헤더 체크 할수 있도록 AuthGuard 추가(client 입장에서는 native sse에서는 동작하지 않고 polyfill에서만 동작함 )

--- src/routes/noti/noti.ts
+++ src/routes/noti/noti.ts
@@ -1,5 +1,4 @@
 import express from 'express';
-import prisma from '@src/prisma';
 import {
   ibDefs,
   asyncWrapper,
@@ -7,138 +6,19 @@ import {
   IBError,
   accessTokenValidCheck,
 } from '@src/utils';
-import { isNil, isNaN, isEmpty } from 'lodash';
+import { isNil, isNaN } from 'lodash';
 
 const notiRouter: express.Application = express();
-// const clients: { sseKey: string; userTokenId: string; res: express.Response }[] = [];
+
 type SSEClientType = {
-  [sseKey: string]: {
-    userTokenId: string;
-    res: express.Response | null;
-  };
+  [userId: string]: express.Response | null;
 };
 
-let sseClients: SSEClientType = {
-  dummySseKey: {
-    userTokenId: 'dummyUserTokneId',
-    res: null,
-  },
-};
-type UserTokenIdToSSEKEyType = {
-  [userTokenId: string]: string;
-};
-let userTokenIdToSSEKey: UserTokenIdToSSEKEyType = {
-  dummyUserTokenId: 'dummySseKey',
+const sseClients: SSEClientType = {
+  '1': null,
 };
 
-(async () => {
-  const sseKeyMap = await prisma.sSEKeyMap.findMany({});
-  if (isEmpty(sseKeyMap)) return;
-  if (!isNil(sseKeyMap[0].sseClients) && !isEmpty(sseKeyMap[0].sseClients))
-    sseClients = {
-      ...sseClients,
-      ...(JSON.parse(sseKeyMap[0].sseClients) as SSEClientType),
-    };
-
-  if (
-    !isNil(sseKeyMap[0].userTokenIdToSSEKey) &&
-    !isEmpty(sseKeyMap[0].userTokenIdToSSEKey)
-  )
-    userTokenIdToSSEKey = {
-      ...userTokenIdToSSEKey,
-      ...(JSON.parse(
-        sseKeyMap[0].userTokenIdToSSEKey,
-      ) as UserTokenIdToSSEKEyType),
-    };
-})().catch((err: Error) => {
-  console.error(err);
-});
-
-/**
- * server sent event를 활용한 채팅형식의 게시판에 사용할 클라이언트 식별 key를 부여하는 api
- * 이 sseKey를 통해 클라이언트는 sse 에 접속된 전체 클라이언트로 브로드캐스팅된 이벤트중 자신의 메시지를 식별한다.
- */
-export type GetSSEKeyRequestType = {};
-export type GetSSEKeySuccessResType = {
-  sseKey: string;
-};
-export type GetSSEKeyResType = Omit<IBResFormat, 'IBparams'> & {
-  IBparams: GetSSEKeySuccessResType | {};
-};
-
-export const getSSEKey = asyncWrapper(
-  async (
-    req: Express.IBTypedReqBody<GetSSEKeyRequestType>,
-    res: Express.IBTypedResponse<GetSSEKeyResType>,
-  ): Promise<void> => {
-    try {
-      const { locals } = req;
-      const userTokenId = (() => {
-        if (locals && locals?.grade === 'member')
-          return locals?.user?.userTokenId;
-        // return locals?.tokenId;
-        throw new IBError({
-          type: 'NOTAUTHORIZED',
-          message: 'member 등급만 접근 가능합니다.',
-        });
-      })();
-      if (!userTokenId) {
-        throw new IBError({
-          type: 'NOTEXISTDATA',
-          message: '정상적으로 부여된 userTokenId를 가지고 있지 않습니다.',
-        });
-      }
-
-      const sseKey = Math.floor(Math.random() * 10000).toString();
-
-      sseClients[sseKey] = {
-        userTokenId,
-        res: null,
-      };
-
-      console.log(`[sseClients]::`, sseKey, sseClients[sseKey]);
-      userTokenIdToSSEKey[userTokenId] = sseKey;
-
-      await prisma.sSEKeyMap.upsert({
-        where: {
-          id: 1,
-        },
-        update: {
-          userTokenIdToSSEKey: JSON.stringify(userTokenIdToSSEKey),
-          sseClients: JSON.stringify(sseClients),
-        },
-        create: {
-          userTokenIdToSSEKey: JSON.stringify(userTokenIdToSSEKey),
-          sseClients: JSON.stringify(sseClients),
-        },
-      });
-      res.json({
-        ...ibDefs.SUCCESS,
-        IBparams: {
-          sseKey,
-        },
-      });
-      return;
-    } catch (err) {
-      if (err instanceof IBError) {
-        if (err.type === 'INVALIDPARAMS') {
-          res.status(400).json({
-            ...ibDefs.INVALIDPARAMS,
-            IBdetail: (err as Error).message,
-            IBparams: {} as object,
-          });
-          return;
-        }
-      }
-
-      throw err;
-    }
-  },
-);
-
-export type SseSubscribeRequestType = {
-  sseKey: string;
-};
+export type SseSubscribeRequestType = {};
 // export type SseSubscribeSuccessResType = {};
 export type SseSubscribeResType = Omit<IBResFormat, 'IBparams'> & {
   IBparams: {};
@@ -150,32 +30,19 @@ export const sseSubscribe = (
 ): void => {
   try {
     const { locals } = req;
-    const userTokenId = (() => {
+    const userId = (() => {
       if (locals && locals?.grade === 'member')
-        return locals?.user?.userTokenId;
+        return locals?.user?.id.toString();
       // return locals?.tokenId;
       throw new IBError({
         type: 'NOTAUTHORIZED',
         message: 'member 등급만 접근 가능합니다.',
       });
     })();
-    if (!userTokenId) {
+    if (isNil(userId)) {
       throw new IBError({
         type: 'NOTEXISTDATA',
-        message: '정상적으로 부여된 userTokenId를 가지고 있지 않습니다.',
-      });
-    }
-    const sseKey = userTokenIdToSSEKey[userTokenId];
-    console.log(
-      `[userTokenIdToSSEKey]:: userTokenId: ${userTokenId}, sseKey=${sseKey}`,
-      userTokenIdToSSEKey,
-    );
-    // const { sseKey } = req.query;
-
-    if (isNil(sseKey)) {
-      throw new IBError({
-        type: 'INVALIDSTATUS',
-        message: 'userTokenId에 대응하는 sseKey가 존재하지 않습니다.',
+        message: '정상적으로 부여된 userId 가지고 있지 않습니다.',
       });
     }
 
@@ -186,18 +53,15 @@ export const sseSubscribe = (
       'Cache-Control': 'no-cache, no-transform',
     };
     res.set(headers);
-    res.write(`sseKey:${sseKey} connected`);
+    res.write(`userId:${userId} connected`);
 
-    sseClients[sseKey] = {
-      ...sseClients[sseKey],
-      res,
-    };
-    console.log(sseClients);
+    sseClients[userId] = res;
+    // console.log(sseClients);
 
     req.on('close', () => {
-      console.log(`sseKey: ${sseKey} closed `);
+      console.log(`userId: ${userId} closed `);
       //   delete clients[Number(sseKey)];
-      delete sseClients[sseKey];
+      delete sseClients[userId];
     });
     return;
   } catch (err) {
@@ -219,7 +83,7 @@ export const sseSubscribe = (
 export type AskBookingAvailableRequestType = {
   date: string;
   numOfPeople: string;
-  toUserTokenId: string;
+  toUserId: string;
 };
 export type AskBookingAvailableSuccessResType = {};
 export type AskBookingAvailableResType = Omit<IBResFormat, 'IBparams'> & {
@@ -236,53 +100,55 @@ export const askBookingAvailable = asyncWrapper(
   ) => {
     try {
       const { locals } = req;
-      const userTokenId = (() => {
+      const userId = (() => {
         if (locals && locals?.grade === 'member')
-          return locals?.user?.userTokenId;
+          return locals?.user?.id.toString();
         // return locals?.tokenId;
         throw new IBError({
           type: 'NOTAUTHORIZED',
           message: 'member 등급만 접근 가능합니다.',
         });
       })();
-      if (!userTokenId) {
+      if (isNil(userId)) {
         throw new IBError({
           type: 'NOTEXISTDATA',
-          message: '정상적으로 부여된 userTokenId를 가지고 있지 않습니다.',
+          message: '정상적으로 부여된 userId를 가지고 있지 않습니다.',
         });
       }
 
       const params = req.body;
-      const { date, numOfPeople, toUserTokenId } = params;
+      const { date, numOfPeople, toUserId } = params;
 
       if (
         isNil(date) ||
         isNil(numOfPeople) ||
         isNaN(Number(numOfPeople)) ||
-        isNil(toUserTokenId)
+        isNil(toUserId)
       ) {
         throw new IBError({
           type: 'INVALIDPARAMS',
           message:
-            '올바른 형식의 date, numOfPeople, toUserTokenId 파라미터가 제공되어야 합니다.',
+            '올바른 형식의 date, numOfPeople, toUserId 파라미터가 제공되어야 합니다.',
         });
       }
 
-      // console.log(userTokenIdToSSEKey);
+      // if (isNil(sseClients[userId])) {
+      //   throw new IBError({
+      //     type: 'INVALIDSTATUS',
+      //     message: 'userId에 해당하는 유저의 sse 연결이 존재하지 않습니다. ',
+      //   });
+      // }
 
-      const sseKey = userTokenIdToSSEKey[toUserTokenId];
-      const fromSSEKey = userTokenIdToSSEKey[userTokenId];
-      if (isNil(sseKey) || isNil(sseClients[sseKey].res)) {
+      if (isNil(sseClients[toUserId])) {
         throw new IBError({
           type: 'INVALIDSTATUS',
-          message:
-            'toUserTokenId에 해당하는 유저의 sse 연결이 존재하지 않습니다. ',
+          message: 'toUserId에 해당하는 유저의 sse 연결이 존재하지 않습니다. ',
         });
       }
-      sseClients[sseKey].res!.write(`id: 00\n`);
-      sseClients[sseKey].res!.write(`event: sseKey${sseKey}\n`);
-      sseClients[sseKey].res!.write(
-        `data: {"message" : "nextAPI:replyForAskBookingAvailable, from:${fromSSEKey}"}\n\n`,
+      sseClients[toUserId]!.write(`id: 00\n`);
+      sseClients[toUserId]!.write(`event: userId${toUserId}\n`);
+      sseClients[toUserId]!.write(
+        `data: {"message" : "nextAPI:replyForAskBookingAvailable, from:${userId}"}\n\n`,
       );
 
       await (async () => {})();
@@ -315,85 +181,6 @@ export const askBookingAvailable = asyncWrapper(
     }
   },
 );
-
-export type ServerReadyEventTestRequestType = {};
-export type ServerReadyEventTestSuccessResType = {};
-export type ServerReadyEventTestResType = Omit<IBResFormat, 'IBparams'> & {
-  IBparams: {};
-};
-
-/**
- *
- */
-export const serverReadyEventTest = asyncWrapper(
-  async (
-    req: Express.IBTypedReqBody<ServerReadyEventTestRequestType>,
-    res: Express.IBTypedResponse<ServerReadyEventTestResType>,
-  ) => {
-    try {
-      const { locals } = req;
-      const userTokenId = (() => {
-        if (locals && locals?.grade === 'member')
-          return locals?.user?.userTokenId;
-        // return locals?.tokenId;
-        throw new IBError({
-          type: 'NOTAUTHORIZED',
-          message: 'member 등급만 접근 가능합니다.',
-        });
-      })();
-      if (!userTokenId) {
-        throw new IBError({
-          type: 'NOTEXISTDATA',
-          message: '정상적으로 부여된 userTokenId를 가지고 있지 않습니다.',
-        });
-      }
-
-      const sseKey = userTokenIdToSSEKey[userTokenId];
-      if (isNil(sseClients[sseKey].res)) {
-        throw new IBError({
-          type: 'INVALIDSTATUS',
-          message: 'sseKey 매핑값 상태에 문제가 있습니다. ',
-        });
-      }
-
-      sseClients[sseKey].res!.write(`sseKey: ${sseKey}\n`);
-      sseClients[sseKey].res!.write(`event: sseKey${sseKey}\n`);
-      sseClients[sseKey].res!.write(
-        `data: {"message" : "hello SSE ${sseKey}!"}\n\n`,
-      );
-
-      await (async () => {})();
-      res.json({
-        ...ibDefs.SUCCESS,
-        IBparams: {},
-      });
-      return;
-    } catch (err) {
-      if (err instanceof IBError) {
-        if (err.type === 'INVALIDPARAMS') {
-          res.status(400).json({
-            ...ibDefs.INVALIDPARAMS,
-            IBdetail: (err as Error).message,
-            IBparams: {} as object,
-          });
-          return;
-        }
-        if (err.type === 'DUPLICATEDDATA') {
-          res.status(409).json({
-            ...ibDefs.DUPLICATEDDATA,
-            IBdetail: (err as Error).message,
-            IBparams: {} as object,
-          });
-          return;
-        }
-      }
-
-      throw err;
-    }
-  },
-);
-
-notiRouter.post('/getSSEKey', accessTokenValidCheck, getSSEKey);
 
 notiRouter.get('/sseSubscribe', accessTokenValidCheck, sseSubscribe);
 
@@ -401,10 +188,5 @@ notiRouter.post(
   '/askBookingAvailable',
   accessTokenValidCheck,
   askBookingAvailable,
-);
-notiRouter.post(
-  '/serverReadyEventTest',
-  accessTokenValidCheck,
-  serverReadyEventTest,
 );
 export default notiRouter;
