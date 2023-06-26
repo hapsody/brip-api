ca28010ebda32d40fa16f85ed6cd206163eebcc1
hjkangIB <hjkang@idealbloom.io>
2023-06-26 18:43:58 +0900

    feat: redisClient 원격 url 접속할수 있도록 파라미터 변경

--- src/redis/index.ts
+++ src/redis/index.ts
@@ -1,5 +1,12 @@
 import { createClient } from 'redis';
+import dotenv from 'dotenv';
 
-const redisClient = createClient();
+dotenv.config();
+
+const redisClient = createClient({
+  url: `redis://${process.env.REDIS_URL as string}:${
+    process.env.REDIS_PORT as string
+  }`,
+});
 
 export default redisClient;
