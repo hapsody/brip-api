24fedcefcadfb6da7e824f3719576e93b6520806
hjkangIB <hjkang@idealbloom.io>
2023-09-26 13:33:09 +0900

    chore: dailyBatch.ts 에서 구글 구독 상태 업데이트 부분 삭제

--- src/timer/daily/dailyBatchJob.ts
+++ src/timer/daily/dailyBatchJob.ts
@@ -1,7 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 // import registTPFromAdPlace from '../scripts/registTPFromAdPlace/registTPFromAdPlace';
 import {
-  validateSubscriptionReceipt,
+  // validateSubscriptionReceipt,
   retrieveLastSubscriptionReceipt,
 } from '@src/utils';
 import moment from 'moment';
@@ -11,14 +11,15 @@ const prisma = new PrismaClient();
 async function batchJob(): Promise<void> {
   // await registTPFromAdPlace();
   const nowTimestamp = new Date().getTime();
-  const googleExpiredSubscriptions =
-    await prisma.googleInAppPurchaseLog.findMany({
-      where: {
-        expiryTime: {
-          lt: Math.ceil(nowTimestamp / 1000),
-        },
-      },
-    });
+  // /adPlace/googleSubscriptionHook api 추가로 인해 (구글 RTDN) 실시간 상태 업데이트가 되어 구글은 별도의 batch 과정이 필요없음
+  // const googleExpiredSubscriptions =
+  //   await prisma.googleInAppPurchaseLog.findMany({
+  //     where: {
+  //       expiryTime: {
+  //         lt: Math.ceil(nowTimestamp / 1000),
+  //       },
+  //     },
+  //   });
 
   const appleExpiredSubscriptions = await prisma.appleInAppPurchaseLog.findMany(
     {
@@ -30,37 +31,37 @@ async function batchJob(): Promise<void> {
     },
   );
 
-  // eslint-disable-next-line no-restricted-syntax
-  for await (const v of googleExpiredSubscriptions) {
-    console.log(`google purchaseToken: ${v.purchaseToken}`);
-    const validationResult = await validateSubscriptionReceipt({
-      purchaseToken: v.purchaseToken,
-    });
+  // // eslint-disable-next-line no-restricted-syntax
+  // for await (const v of googleExpiredSubscriptions) {
+  //   console.log(`google purchaseToken: ${v.purchaseToken}`);
+  //   const validationResult = await validateSubscriptionReceipt({
+  //     purchaseToken: v.purchaseToken,
+  //   });
 
-    if (moment().diff(moment(Number(validationResult.expiryTimeMillis))) >= 0) {
-      /// expired
-      await prisma.adPlace.update({
-        where: {
-          id: v.adPlaceId,
-        },
-        data: {
-          subscribe: false,
-        },
-      });
-    } else {
-      /// not expired, revised expiryTime
-      await prisma.googleInAppPurchaseLog.update({
-        where: {
-          id: v.id,
-        },
-        data: {
-          expiryTime: Math.ceil(
-            Number(validationResult.expiryTimeMillis) / 1000,
-          ),
-        },
-      });
-    }
-  }
+  //   if (moment().diff(moment(Number(validationResult.expiryTimeMillis))) >= 0) {
+  //     /// expired
+  //     await prisma.adPlace.update({
+  //       where: {
+  //         id: v.adPlaceId,
+  //       },
+  //       data: {
+  //         subscribe: false,
+  //       },
+  //     });
+  //   } else {
+  //     /// not expired, revised expiryTime
+  //     await prisma.googleInAppPurchaseLog.update({
+  //       where: {
+  //         id: v.id,
+  //       },
+  //       data: {
+  //         expiryTime: Math.ceil(
+  //           Number(validationResult.expiryTimeMillis) / 1000,
+  //         ),
+  //       },
+  //     });
+  //   }
+  // }
 
   // eslint-disable-next-line no-restricted-syntax
   for await (const v of appleExpiredSubscriptions) {
