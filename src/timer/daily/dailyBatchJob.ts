bd784aed782cc2e532548145973e51310e0a1189
hjkangIB <hjkang@idealbloom.io>
2023-09-27 17:57:11 +0900

    chore: dailyBatch 스크립트에서 인앱 구독 상태 업데이트 관련 로직 제거 (실시간으로 변경함)

--- src/timer/daily/dailyBatchJob.ts
+++ src/timer/daily/dailyBatchJob.ts
@@ -1,16 +1,16 @@
 import { PrismaClient } from '@prisma/client';
 // import registTPFromAdPlace from '../scripts/registTPFromAdPlace/registTPFromAdPlace';
-import {
-  // validateSubscriptionReceipt,
-  retrieveLastSubscriptionReceipt,
-} from '@src/utils';
-import moment from 'moment';
+// import {
+//   // validateSubscriptionReceipt,
+//   retrieveLastSubscriptionReceipt,
+// } from '@src/utils';
+// import moment from 'moment';
 
 const prisma = new PrismaClient();
 
 async function batchJob(): Promise<void> {
   // await registTPFromAdPlace();
-  const nowTimestamp = new Date().getTime();
+  // const nowTimestamp = new Date().getTime();
   // /adPlace/googleSubscriptionHook api 추가로 인해 (구글 RTDN) 실시간 상태 업데이트가 되어 구글은 별도의 batch 과정이 필요없음
   // const googleExpiredSubscriptions =
   //   await prisma.googleInAppPurchaseLog.findMany({
@@ -20,24 +20,21 @@ async function batchJob(): Promise<void> {
   //       },
   //     },
   //   });
-
-  const appleExpiredSubscriptions = await prisma.appleInAppPurchaseLog.findMany(
-    {
-      where: {
-        expiresDate: {
-          lt: Math.ceil(nowTimestamp / 1000),
-        },
-      },
-    },
-  );
-
+  // const appleExpiredSubscriptions = await prisma.appleInAppPurchaseLog.findMany(
+  //   {
+  //     where: {
+  //       expiresDate: {
+  //         lt: Math.ceil(nowTimestamp / 1000),
+  //       },
+  //     },
+  //   },
+  // );
   // // eslint-disable-next-line no-restricted-syntax
   // for await (const v of googleExpiredSubscriptions) {
   //   console.log(`google purchaseToken: ${v.purchaseToken}`);
   //   const validationResult = await validateSubscriptionReceipt({
   //     purchaseToken: v.purchaseToken,
   //   });
-
   //   if (moment().diff(moment(Number(validationResult.expiryTimeMillis))) >= 0) {
   //     /// expired
   //     await prisma.adPlace.update({
@@ -62,42 +59,40 @@ async function batchJob(): Promise<void> {
   //     });
   //   }
   // }
-
   // eslint-disable-next-line no-restricted-syntax
-  for await (const v of appleExpiredSubscriptions) {
-    console.log(`apple originalTransactionId: ${v.originalTransactionId}`);
-    const validationResult = await retrieveLastSubscriptionReceipt(
-      v.originalTransactionId,
-    );
-
-    if (
-      moment().diff(
-        moment(Number(validationResult.transactionInfo.expiresDate)),
-      ) >= 0
-    ) {
-      /// expired
-      await prisma.adPlace.update({
-        where: {
-          id: v.adPlaceId,
-        },
-        data: {
-          subscribe: false,
-        },
-      });
-    } else {
-      /// not expired, revised expiryTime
-      await prisma.appleInAppPurchaseLog.update({
-        where: {
-          id: v.id,
-        },
-        data: {
-          expiresDate: Math.ceil(
-            Number(validationResult.transactionInfo.expiresDate) / 1000,
-          ),
-        },
-      });
-    }
-  }
+  // for await (const v of appleExpiredSubscriptions) {
+  //   console.log(`apple originalTransactionId: ${v.originalTransactionId}`);
+  //   const validationResult = await retrieveLastSubscriptionReceipt(
+  //     v.originalTransactionId,
+  //   );
+  //   if (
+  //     moment().diff(
+  //       moment(Number(validationResult.transactionInfo.expiresDate)),
+  //     ) >= 0
+  //   ) {
+  //     /// expired
+  //     await prisma.adPlace.update({
+  //       where: {
+  //         id: v.adPlaceId,
+  //       },
+  //       data: {
+  //         subscribe: false,
+  //       },
+  //     });
+  //   } else {
+  //     /// not expired, revised expiryTime
+  //     await prisma.appleInAppPurchaseLog.update({
+  //       where: {
+  //         id: v.id,
+  //       },
+  //       data: {
+  //         expiresDate: Math.ceil(
+  //           Number(validationResult.transactionInfo.expiresDate) / 1000,
+  //         ),
+  //       },
+  //     });
+  //   }
+  // }
 }
 
 function wrapper(func: () => Promise<void>): () => void {
